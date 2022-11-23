31d1501d35eb22aa83c0a744deb6a9a13c5d38f4
dallafior <lauredallafior@MacBook-Pro-de-laure.local>
2022-11-23 16:10:19 +0100

    add test fizzbuzz

---
+++ fizzbuzz/index.js
@@ -0,0 +1,6 @@
+for (let number = 1; number < 1000; number++) {
+    if (number % 15 === 0) console.log("FizzBuzz");
+    else if (number % 3 === 0) console.log("Fizz");
+    else if (number % 5 === 0) console.log("Buzz");
+    else console.log(number);
+}
